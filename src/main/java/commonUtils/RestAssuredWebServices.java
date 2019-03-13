e462edb411c4d9fd49193af4c2a3a373e178a1f7
Ashish Sharma <Ashish Sharma@ASHISH-PC>
2019-03-13 23:56:05 +0530

    Rest Assured Changes

--- src/main/java/commonUtils/RestAssuredWebServices.java
+++ src/main/java/commonUtils/RestAssuredWebServices.java
@@ -1,9 +1,46 @@
 package commonUtils;
 
+import java.util.List;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 public class RestAssuredWebServices {
 
+	String uri="";
+	RequestSpecification httpRequest;
+	Response response;
+	String responseBody;
+	Headers headerList;
+
+	//Constructor to set the base URi
+	RestAssuredWebServices(String uri){
+		this.uri=uri;
+		RestAssured.baseURI=uri;
+	}
 	
+	//Get method for the given name
+	public void get(String name) {
+	 httpRequest=RestAssured.given();
+	 response = httpRequest.request(Method.GET, name);
+	}
 	
+	//getting the response body
+	public String getRequestBody() {
+		responseBody=response.getBody().asString();
+		return responseBody;
+	}
+	
+	public int getStatus() {
+		return response.getStatusCode();
+	}
+	
+	public void getHeaders() {
+		  headerList=response.getHeaders();
+	}
 	
 	
 }
